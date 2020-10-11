a7b0f48fb1bfa882c70f2fb885ef7aabd9cfe47d
quincyzhuang <quincy.zhuang@gmail.com>
2020-10-11 10:30:33 -0500

    Remove archive

--- archive/bashtemp.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-while true;do cat temp.txt && sleep 10;done
--- archive/promstart.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-screen -d -m /etc/prometheus/prometheus --config.file /etc/prometheus/prometheus.yml
-screen -d -m /etc/pushgateway/pushgateway
