5a700d62a8dbe553995982071e2d27c2d2173936
quincyzhuang <37846942+quincyzhuang@users.noreply.github.com>
2020-10-11 20:23:52 -0500

    Update readProbe.py

--- python/readProbe.py
+++ python/readProbe.py
@@ -8,24 +8,60 @@ import time
 import sys
 
 FILE_PATH = "temp.txt"
-deviceMAC = "00:35:ff:d8:64:54"
-setUUID = "0000fff1-0000-1000-8000-00805f9b34fb"
-authUUID = "0000fff2-0000-1000-8000-00805f9b34fb"
-rtUUID = "0000fff4-0000-1000-8000-00805f9b34fb"
-setDataUUID = "0000fff5-0000-1000-8000-00805f9b34fb"
-historyUUID = "0000fff3-0000-1000-8000-00805f9b34fb"
-notify = b"\x01\x00"
-authData = bytearray(b'\x21\x07\x06\x05\x04\x03\x02\x01\xb8\x22\0\0\0\0\0')
-rtEnable = bytearray(b'\x0b\x01\0\0\0\0')
-batteryEnable = bytearray(b'\x08\x24\0\0\0\0')
+DEVICE_MAC = "00:35:ff:d8:64:54"
+SET_UUID = "0000fff1-0000-1000-8000-00805f9b34fb"
+AUTH_UUID = "0000fff2-0000-1000-8000-00805f9b34fb"
+RT_UUID = "0000fff4-0000-1000-8000-00805f9b34fb"
+SETDATA_UUID = "0000fff5-0000-1000-8000-00805f9b34fb"
+HISTORY_UUID = "0000fff3-0000-1000-8000-00805f9b34fb"
+NOTIFY = b"\x01\x00"
+AUTH_DATA = bytearray(b'\x21\x07\x06\x05\x04\x03\x02\x01\xb8\x22\0\0\0\0\0')
+RT_ENABLE = bytearray(b'\x0b\x01\0\0\0\0')
+BATTERY_ENABLE = bytearray(b'\x08\x24\0\0\0\0')
 LEGACY_MODE=0
 
+class IbbqDevice():
+    # Parameters
+    #   device = pointer to bluepy device
+    #   characteristics = dictionary of characteristics
+    #   connected = boolean indicating connection status
+    #   probe1/2 = temperature of corresponding probe
+    #   batterylevel = current battery level
+    def __init__(self,device,char_dict):
+        self.device = device
+        self.characteristics = char_dict
+        self.connected = True 
+
+    def setTemperature(self,probe1,probe2):
+        self.probe1 = probe1
+        self.probe2 = probe2
+
+    def setBatteryLevel(self,pct):
+        self.batterylevel = pct
+
+    def disconnect():
+        self.connected = False
+        self.device.disconnect()
+
+    def isConnected():
+        return self.connected
+
+    def getTemperature():
+        return (self.probe1,self.probe2)
+
+    def getBatteryLevel():
+        return self.batterylevel
+
+    def getCharacteristics():
+        return self.characteristics
+
+    def getDevice():
+        return self.device
 
 class MyDelegate(btle.DefaultDelegate):
     def __init__(self):
         btle.DefaultDelegate.__init__(self)
         # ... initialise here
-
     def handleNotification(self, cHandle, data):
         result = list()
         if cHandle == 37:
@@ -55,63 +91,67 @@ class MyDelegate(btle.DefaultDelegate):
                 push_temperature(probe_one,1)
                 push_temperature(probe_two,2)
 
-print("Connecting...")
+def connect():
+    for i in range(0,5):
+        try:
+            dev = btle.Peripheral(DEVICE_MAC)
+            dev_chars = dict()
+            print("Connected to " + str(dev.addr))
+            #
+            dev_chars["auth"] = dev.getCharacteristics(uuid=AUTH_UUID)[0] # fff2 authentication characteristic
+            dev_chars["rt"] = dev.getCharacteristics(uuid=RT_UUID)[0]
+            dev_chars["set"] = dev.getCharacteristics(uuid=SET_UUID)[0]
+            dev_chars["setD"] = dev.getCharacteristics(uuid=SETDATA_UUID)[0]
+            dev_chars["histD"] = dev.getCharacteristics(uuid=HISTORY_UUID)[0]
+            ibbq_device = IbbqDevice(dev,dev_chars)
+            #
+            dev_chars["auth"].write(AUTH_DATA,withResponse=True)
+            dev.writeCharacteristic(rt.getHandle() + 1,NOTIFY)
+            dev.writeCharacteristic(set.getHandle() + 1,NOTIFY)
+            dev_chars["setD"].write(BATTERY_ENABLE)
+            dev_chars["setD"].write(RT_ENABLE)
+            print("Authenticated successfully!")
+        except BTLEException:
+            print("Bluepy exception. Retrying connection. Attempt " + str(i) + " of 5")
+            continue
+        except BrokenPipeError as e:
+            print("IO exception. Retrying connection. Attempt " + str(i) + " of 5")
+            continue
+        except Exception as e:
+            print(str(type(e)) + " exception. Retrying connection. Attempt " + str(i) + " of 5")
+            continue
+        break
+    else:
+        print("All connection attempts failed.")
+        if ibbq_device:
+            ibbq_device.disconnect()
+    return ibbq_device
 
-for i in range(0,5):
-    try:
-        dev = ''
-        dev = btle.Peripheral(deviceMAC)
-        print("Connected to " + str(dev.addr)+ "!")
-        auth = dev.getCharacteristics(uuid=authUUID)[0] # fff2 authentication characteristic
-        rt = dev.getCharacteristics(uuid=rtUUID)[0]
-        set = dev.getCharacteristics(uuid=setUUID)[0]
-        setD = dev.getCharacteristics(uuid=setDataUUID)[0]
-        histD = dev.getCharacteristics(uuid=historyUUID)[0]
-        print("Authentication UUID: " + str(auth.uuid))
-        print("RealTime UUID: " + str(rt.uuid))
-        print("SettingResult UUID: " + str(set.uuid))
-        print("SettingData UUID: " + str(setD.uuid))
-        print("HistoryData UUID: " + str(histD.uuid))
-        auth.write(authData,withResponse=True)
-        dev.writeCharacteristic(rt.getHandle() + 1,notify)
-        dev.writeCharacteristic(set.getHandle() + 1,notify)
-        setD.write(batteryEnable)
-        setD.write(rtEnable)
-        print("Authenticated successfully!")
-    except BTLEException:
-        print("Bluepy exception. Retrying connection. Attempt " + str(i) + " of 5")
-        continue
-    except BrokenPipeError as e:
-        print("IO exception. Retrying connection. Attempt " + str(i) + " of 5")
-        continue
-    except Exception as e:
-        print(str(type(e)) + " exception. Retrying connection. Attempt " + str(i) + " of 5")
-        continue
-    break
-else:
-    print("All attempts failed. Please try again.")
-    if dev:
+def pollData(device_object):
+    dev = device_object.getDevice()
+    dev.setDelegate(MyDelegate())
+    for i in range(0,5):
+        try:
+            while True:
+                if dev.waitForNotifications(1):
+                    continue
+        except BTLEException:
+            print("Retrying data transfer. Attempt " + str(i) + " of 5")
+            continue
+        except AttributeError:
+            print("Retrying data transfer. Attempt " + str(i) + " of 5")
+            continue
+        except Exception as e:
+            print(str(type(e)) + "Retrying data transfer. Attempt " + str(i) + " of 5") 
+            continue
+    else:
+        print("Data transfer failure.")
         dev.disconnect()
-    sys.exit()
 
-print("Waiting for data...")
-
-dev.setDelegate(MyDelegate())
-for i in range(0,5):
-    try:
-        while True:
-            if dev.waitForNotifications(1):
-                continue
-    except BTLEException:
-        print("Retrying data transfer. Attempt " + str(i) + " of 5")
+while True:
+    print("Connecting...")
+    my_device = connect()
+    if my_device.isConnected() != True:
         continue
-    except AttributeError:
-        print("Retrying data transfer. Attempt " + str(i) + " of 5")
-        continue
-    except Exception as e:
-        print(str(type(e)) + "Retrying data transfer. Attempt " + str(i) + " of 5") 
-        continue
-else:
-    print("Data transfer failure. Please try again.")
-    dev.disconnect()
-    raise SystemExit
+    print("Collecting data...")
+    pollData(my_device)
